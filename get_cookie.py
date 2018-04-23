b3f5756748d86fa30eca8e49c7b53a5db0dbaa02
Michael Reichert <michael.reichert@geofabrik.de>
2018-04-23 18:17:43 +0200

    add an API endpoint and a client to retrieve cookies in an automated way

---
+++ get_cookie.py
@@ -0,0 +1,82 @@
+#! /usr/bin/env python3
+
+import sys
+
+import urllib.parse
+import base64
+import json
+import nacl.utils
+import requests
+from requests_oauthlib import OAuth1
+
+from sendfile_osm_oauth_protector.config import Config
+from sendfile_osm_oauth_protector.key_manager import KeyManager
+from sendfile_osm_oauth_protector.oauth_data_cookie import OAuthDataCookie
+
+config = Config()
+key_manager = KeyManager(config.KEY_DIR)
+
+
+def respond_error(http_error_message, start_response, message):
+    msg = message.encode("utf8")
+    response_headers = [("Content-type", "text/plain; charset=utf-8"),
+                        ("Content-Length", str(len(msg)))]
+    start_response(http_error_message, response_headers)
+    return [msg]
+
+
+def get_request_token(environ, start_response):
+    oauth = OAuth1(config.CLIENT_KEY, client_secret=config.CLIENT_SECRET)
+    r = requests.post(url=config.REQUEST_TOKEN_URL, auth=oauth)
+    parts = urllib.parse.parse_qs(r.text)
+
+    crypto_box = key_manager.boxes[config.KEY_NAME]
+    token_secret = parts["oauth_token_secret"][0]
+    nonce = nacl.utils.random(nacl.public.Box.NONCE_SIZE)
+    token_secret_encr = base64.b64encode(crypto_box.encrypt(token_secret.encode("utf8"), nonce)).decode("ascii")
+
+    result = {"oauth_token": parts["oauth_token"][0],
+              "oauth_token_secret_encr": token_secret_encr,
+              "authorization_url": config.AUTHORIZATION_URL}
+    result_enc = json.dumps(result).encode("utf-8")
+    response_headers = [("Content-type", "application/json, charset=utf-8"),
+                        ("Content-Length", str(len(result_enc)))]
+    start_response("200 OK", response_headers)
+    return [result_enc]
+
+
+def get_access_token(environ, start_response):
+    # parse POST data
+    try:
+        request_body_size = int(environ.get("CONTENT_LENGTH", 0))
+    except ValueError:
+        request_body_size = 0
+    if request_body_size <= 0:
+        return respond_error("400 Bad Request", start_response, "Missing or unreadable 'Content-Length' header")
+    request_body = environ["wsgi.input"].read(request_body_size).decode()
+
+    new_environ = {"QUERY_STRING": request_body}
+    oauth_data_cookie = OAuthDataCookie(config, new_environ, key_manager)
+    oauth_data_cookie.get_access_token_from_api()
+    if not oauth_data_cookie.check_with_osm_api():
+        return respond_error("502 Bad Gateway", start_response, "Failed to verify if you are allowed to access the protected resources")
+    cookie = oauth_data_cookie.output().encode("ascii")
+
+    response_headers = [("Content-type", "text/plain; charset=ascii"),
+                        ("Content-length", str(len(cookie)))]
+    start_response("200 OK", response_headers)
+    return [cookie]
+
+
+def application(environ, start_response):
+    if environ["REQUEST_METHOD"] != "POST":
+        return respond_error("400 Bad Request", start_response, "Only POST requests are permitted")
+    params = urllib.parse.parse_qs(environ["QUERY_STRING"])
+    action = params.get("action", None)
+    if action is None:
+        return respond_error("400 Bad Request", start_response, "Parameter 'action' is missing")
+    if action[0] == "request_token":
+        return get_request_token(environ, start_response)
+    elif action[0] == "get_access_token_cookie":
+        return get_access_token(environ, start_response)
+    return respond_error("400 Bad Request", start_response, "The requested 'action' is not supported.")
---
+++ oauth_cookie_client.py
@@ -0,0 +1,116 @@
+#! /usr/bin/env python3
+
+import argparse
+import json
+import re
+import requests
+import sys
+
+CUSTOM_HEADER = {"user-agent": "oauth_cookie_client.py"}
+
+def report_error(message):
+    sys.stderr.write("{}\n".format(message))
+    exit(1)
+
+
+def find_authenticity_token(response):
+    """
+    Search the authenticity_token in the response of the server
+    """
+    pattern = r"name=\"csrf-token\" content=\"([^\"]+)\""
+    m = re.search(pattern, response)
+    if m is None:
+        report_error("Could not find the authenticity_token in the website to be scraped.")
+    try:
+        return m.group(1)
+    except IndexError:
+        sys.stderr.write("ERROR: The login form does not contain an authenticity_token.\n")
+        exit(1)
+
+
+parser = argparse.ArgumentParser(description="Get a cookie to access service protected by OpenStreetMap OAuth 1.0a and osm-internal-oauth")
+parser.add_argument("-o", "--output", default=None, help="write the cookie to the specified file instead to STDOUT", type=open)
+parser.add_argument("-u", "--user", default=None, help="user name", type=str)
+parser.add_argument("-p", "--password", default=None, help="Password, leave empty to force input from STDIN.", type=str)
+parser.add_argument("-s", "--settings", default=None, help="JSON file containing parameters", type=open)
+parser.add_argument("-c", "--consumer-url", default=None, help="URL of the OAuth cookie generation API of the provider who provides you OAuth protected access to their ressources", type=str)
+parser.add_argument("--osm-host", default="https://www.openstreetmap.org/", help="hostname of the OSM API/website to use (e.g. 'www.openstreetmap.org' or 'master.apis.dev.openstreetmap.org')", type=str)
+
+
+args = parser.parse_args()
+settings = {}
+if args.settings is not None:
+    with open(args.settings, "rb") as settingsfile:
+        settings = json.load(settingsfile)
+
+username = settings.get("user", args.user)
+if username is None:
+    username = input("Please enter your user name and press ENTER:")
+if username is None:
+    report_error("The username must not be empty.")
+password = settings.get("password", args.password)
+if password is None:
+    password = input("Please enter your password and press ENTER:")
+if password is None:
+    report_error("The password must not be empty.")
+
+osm_host = settings.get("osm_host", args.osm_host)
+consumer_url = settings.get("consumer_url", args.consumer_url)
+if consumer_url is None:
+    report_error("No consumer URL provided")
+
+# get request token
+url = args.consumer_url + "?action=request_token"
+r = requests.post(url, data={}, headers=CUSTOM_HEADER)
+if r.status_code != 200:
+    report_error("POST {}, received HTTP status code {} but expected 200".format(url, r.status_code))
+json_response = json.loads(r.text)
+authorize_url = osm_host + "/oauth/authorize"
+try:
+    oauth_token = json_response["oauth_token"]
+    oauth_token_secret_encr = json_response["oauth_token_secret_encr"]
+except KeyError:
+    report_error("oauth_token was not found in the first response by the consumer")
+
+# get OSM session
+login_url = osm_host + "/login?cookie_test=true"
+s = requests.Session()
+r = s.get(login_url, headers=CUSTOM_HEADER)
+if r.status_code != 200:
+    report_error("GET {}, received HTTP code {}".format(login_url, r.status_code))
+
+# login
+authenticity_token = find_authenticity_token(r.text)
+login_url = osm_host + "/login"
+r = s.post(login_url, data={"username": username, "password": password, "referer": "/", "commit": "Login", "authenticity_token": authenticity_token}, allow_redirects=False, headers=CUSTOM_HEADER)
+if r.status_code != 302:
+    report_error("POST {}, received HTTP code {} but expected 302".format(login_url, r.status_code))
+
+# authorize
+authorize_url = "{}/oauth/authorize?oauth_token={}".format(osm_host, oauth_token)
+r = s.get(authorize_url, headers=CUSTOM_HEADER)
+if r.status_code != 200:
+    report_error("GET {}, received HTTP code {} but expected 200".format(authorize_url, r.status_code))
+authenticity_token = find_authenticity_token(r.text)
+
+post_data = {"oauth_token": oauth_token, "oauth_callback": "", "authenticity_token": authenticity_token, "allow_read_prefs": "yes", "commit": "Save changes"}
+authorize_url = "{}/oauth/authorize".format(osm_host)
+r = s.post(authorize_url, data=post_data, headers=CUSTOM_HEADER)
+if r.status_code != 200:
+    report_error("POST {}, received HTTP code {} but expected 200".format(authorize_url, r.status_code))
+
+# logout
+logout_url = "{}/logout".format(osm_host)
+r = s.get(logout_url, headers=CUSTOM_HEADER)
+if r.status_code != 200 and r.status_code != 302:
+    report_error("POST {}, received HTTP code {} but expected 200 or 302".format(logout_url))
+
+# get final cookie
+url = args.consumer_url + "?action=get_access_token_cookie"
+r = requests.post(url, data={"oauth_token": oauth_token, "oauth_token_secret_encr": oauth_token_secret_encr}, headers=CUSTOM_HEADER)
+
+if not args.output:
+    sys.stdout.write(r.text)
+else:
+    with open(args.output, "w") as outfile:
+        outfile.write(r.text)
